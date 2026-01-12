ff7dc6798357df3fc68c854999195021be3de469
Ari Karan <arikaran4044@gmail.com>
2026-01-12 15:11:42 +0530

    try to add in GitHub

---
+++ Arr.java
@@ -0,0 +1,8 @@
+class Arr{
+    public static void main(String[] args){
+        int[] arr={5,10,15};
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
+}
